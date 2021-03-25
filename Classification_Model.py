0efd145c85555c96d80cc2a50a9cccb16087686e
bhanuteja2001 <k.bhanuteja2001@gmail.com>
2021-03-25 17:56:06 +0530

    changed weights path

--- Classification_Model.py
+++ Classification_Model.py
@@ -12,7 +12,7 @@ predicts the type of WBC and returns the result
 class Classification:
     def classification_type(self, image_org):
         #image_orig = Image.open(uploaded_file).convert('RGB')
-        MODEL_PATH = 'E:\Microscopic Insights\Microscopic\Microscopic Images\Microscopic-Images-Insights\weights\model_2.h5'
+        MODEL_PATH = 'E:\Microscopic Insights\Microscopic\Microscopic Images\Microscopic-Images-Insights\model_2.h5'
         model = keras.models.load_model(MODEL_PATH)
         img_array = np.array(image_org)
         img_array = cv2.resize(img_array, (320, 240), interpolation=cv2.INTER_NEAREST)  # norm
