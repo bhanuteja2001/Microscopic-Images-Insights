97f984a866d5e065875c4dde93ef07bdc89d278c
Bhanu Teja <44323155+bhanuteja2001@users.noreply.github.com>
2021-03-26 14:38:39 +0530

    Update Classification_Model.py

--- Classification_Model.py
+++ Classification_Model.py
@@ -13,7 +13,7 @@ class Classification:
     def classification_type(self, image_org):
         #image_orig = Image.open(uploaded_file).convert('RGB')
        # MODEL_PATH = 'E:\Microscopic Insights\Microscopic\Microscopic Images\Microscopic-Images-Insights\weights\model_2.h5'
-        MODEL_PATH = os.getcwd() + '\weights\model_2.h5'
+        MODEL_PATH = os.getcwd() + '/weights/model_2.h5'
         model = keras.models.load_model(MODEL_PATH)
         img_array = np.array(image_org)
         img_array = cv2.resize(img_array, (320, 240), interpolation=cv2.INTER_NEAREST)  # norm
